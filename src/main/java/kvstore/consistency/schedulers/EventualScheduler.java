a3ffaf061fb98df17a80bc5c07551b7f228de4dd
Bo Feng <bfeng1216@gmail.com>
2020-05-03 12:03:56 -0700

    two more schedulers

---
+++ src/main/java/kvstore/consistency/schedulers/EventualScheduler.java
@@ -0,0 +1,105 @@
+package kvstore.consistency.schedulers;
+
+import kvstore.consistency.bases.Scheduler;
+import kvstore.consistency.bases.TaskEntry;
+import kvstore.consistency.tasks.BcastWriteTask;
+import kvstore.consistency.tasks.WriteTask;
+import kvstore.consistency.timestamps.VectorTimestamp;
+import kvstore.servers.Worker;
+
+import java.util.Vector;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class EventualScheduler extends Scheduler<VectorTimestamp> {
+    private int workerId;
+    private LinkedBlockingDeque<TaskEntry<VectorTimestamp>> tasksQ;
+
+    public EventualScheduler(VectorTimestamp ts, int workerId) {
+        super(ts);
+        this.tasksQ = new LinkedBlockingDeque<>(1024);
+        this.workerId = workerId;
+        Worker.logger.info(String.format("%s", this.globalTs.value.toString()));
+    }
+
+    @Override
+    public void run() {
+        try {
+            do {
+                // Thread.sleep(1 * 1000);
+                TaskEntry<VectorTimestamp> task = this.tasksQ.take();
+                if (ifAllowDeliver(task)) {
+                    if (task instanceof BcastWriteTask) {
+                        /* Set the timstamp when multicast the write task */
+                        int senderId = ((BcastWriteTask<VectorTimestamp>) task).workerId;
+
+                        ((BcastWriteTask<VectorTimestamp>) (task)).setTimestamp(incrementAndGetTimeStamp());
+                        Worker.logger.info(String.format("<<<Message%s[%d] Delivered!>>> at %s[%d] | Mode: Causal",
+                                task.ts.value.toString(), senderId, this.getCurrentTimestamp().value.toString(), workerId));
+                    } else if (task instanceof WriteTask) {
+                        int senderId = ((WriteTask<VectorTimestamp>) task).writeReqBcast.getSender();
+
+                        int old = this.globalTs.value.get(senderId);
+                        this.globalTs.value.set(senderId, old + 1);
+                        Worker.logger.info(String.format("<<<Message%s[%d] Delivered!>>> at %s[%d] | Mode: Causal",
+                                task.ts.value.toString(), senderId, this.getCurrentTimestamp().value.toString(), workerId));
+                    }
+
+                    Thread taskThread = new Thread(task);
+                    taskThread.start();
+                    taskThread.join();
+
+                } else {
+                    Worker.logger.info(String.format("Message%s Blocked!", task.ts.value.toString()));
+                    this.tasksQ.put(task);
+                }
+            } while (true);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Allow any task is the next expected
+     */
+    @Override
+    public synchronized boolean ifAllowDeliver(TaskEntry<VectorTimestamp> task) {
+        return true;
+    }
+
+    @Override
+    public synchronized TaskEntry<VectorTimestamp> addTask(TaskEntry<VectorTimestamp> taskEntry) {
+        // if (!(taskEntry instanceof BcastWriteTask)) {
+        // Worker.logger.info(String.format("Add a write task: %s",
+        // taskEntry.ts.value.toString()));
+        // }
+        try {
+            this.tasksQ.put(taskEntry);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public synchronized VectorTimestamp incrementAndGetTimeStamp() {
+        int old = this.globalTs.value.get(this.workerId);
+        this.globalTs.value.set(this.workerId, old + 1);
+        VectorTimestamp newVts = new VectorTimestamp(this.workerId);
+        newVts.value = new Vector<Integer>(this.globalTs.value);
+        return newVts;
+    }
+
+    @Override
+    public synchronized VectorTimestamp updateAndIncrementTimeStamp(int SenderTimeStamp) {
+        return null;
+    }
+
+    @Override
+    public VectorTimestamp getCurrentTimestamp() {
+        VectorTimestamp vts = new VectorTimestamp(0);
+        vts.value = new Vector<Integer>(this.globalTs.value);
+        return vts;
+    }
+}
---
+++ src/main/java/kvstore/consistency/schedulers/LinearScheduler.java
@@ -0,0 +1,147 @@
+package kvstore.consistency.schedulers;
+
+import kvstore.consistency.bases.Scheduler;
+import kvstore.consistency.bases.TaskEntry;
+import kvstore.consistency.tasks.WriteTask;
+import kvstore.consistency.timestamps.ScalarTimestamp;
+import kvstore.servers.Worker;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class LinearScheduler extends Scheduler<ScalarTimestamp> {
+    protected PriorityBlockingQueue<TaskEntry<ScalarTimestamp>> tasksQ;
+    private ConcurrentHashMap<String, Boolean[]> acksMap;
+    private int ackLimit;
+
+    public LinearScheduler(ScalarTimestamp ts, int ackLimit) throws SecurityException, IOException {
+        super(ts);
+        /* A hashmap contains all happened acknowledgement */
+        this.tasksQ = new PriorityBlockingQueue<TaskEntry<ScalarTimestamp>>(1024);
+        this.acksMap = new ConcurrentHashMap<String, Boolean[]>(1024);
+        this.ackLimit = ackLimit;
+    }
+
+    /**
+     * Grab a task from the priority queue, running when received all acks
+     */
+    @Override
+    public void run() {
+        do {
+            try {
+                // Thread.sleep(new Random().nextInt(2) * 1000); /* Random dealy. Only for the
+                // testing purpose */
+
+                /* Taking a task from the queue. Block when the queue is empty */
+                WriteTask<ScalarTimestamp> task = (WriteTask<ScalarTimestamp>) tasksQ.take();
+
+                /* For debugging */
+                // logger.info(String.format("<<<Run Task %s: Message[%d][%d]>>>",
+                // task.getClass().getName(),
+                // task.localClock, task.id));
+
+                /* Here it only allows broadcasting once for each message */
+                if (task.getBcastCount() == 0)
+                    task.bcastAcks();
+
+                /* Deliver the message when all replies received */
+                if (ifAllowDeliver(task)) {
+                    task.abortBcastAckTask();
+                    deleteAckArr(task.getTaskId());
+
+                    Thread taskThread = new Thread(task);
+                    taskThread.start();
+                    taskThread.join();
+                    Worker.logger
+                            .info(String.format("<<<Message[%d][%d] Delivered!>>> | Mode: Sequential", task.ts.localClock, task.ts.id));
+
+                } else {
+
+                    tasksQ.put(task); /* Put back the task for rescheduling */
+
+                    // /* For debugging */
+                    // logger.info(String.format("<<<Message[%d][%d] Blocked! Current ack
+                    // array:%s>>>", task.localClock,
+                    // task.id, Arrays.toString(this.acksMap.get(task.toString()))));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } while (true);
+    }
+
+    /**
+     * Add a new task and also create the corresponding ackMap item for this message
+     */
+    @Override
+    public synchronized TaskEntry<ScalarTimestamp> addTask(TaskEntry<ScalarTimestamp> newTask) {
+        tasksQ.put(newTask); /* Put the taks to the priority queue */
+        if (!this.acksMap.containsKey(newTask.getTaskId())) {
+            Boolean[] ackArr = new Boolean[ackLimit];
+            Arrays.fill(ackArr, false);
+            this.acksMap.put(newTask.getTaskId(), ackArr);
+        }
+        return newTask;
+    }
+
+    /**
+     * Check if all replies for this message is received
+     */
+    @Override
+    public synchronized boolean ifAllowDeliver(TaskEntry<ScalarTimestamp> task) {
+        String key = task.getTaskId();
+        if (!this.acksMap.containsKey(key) || Arrays.asList(this.acksMap.get(key)).contains(false))
+            return false;
+        return true;
+    }
+
+    /**
+     * Update the ack for the specified message represented by the key. This method
+     * must be synchronized because it can be accessed by multiple threads
+     *
+     * @param ackReq an acknowledgement request
+     * @return
+     */
+    public synchronized Boolean[] updateAck(ScalarTimestamp ts, int sender) {
+        String key = ts.genKey();
+        if (!this.acksMap.containsKey(key)) {
+            Boolean[] ackArr = new Boolean[ackLimit];
+            Arrays.fill(ackArr, false);
+            this.acksMap.put(key, ackArr);
+            this.acksMap.get(key)[sender] = true;
+        } else {
+            this.acksMap.get(key)[sender] = true;
+        }
+        return this.acksMap.get(key);
+    }
+
+    public synchronized void deleteAckArr(String key) {
+        this.acksMap.remove(key);
+    }
+
+    @Override
+    public synchronized ScalarTimestamp incrementAndGetTimeStamp() {
+        (this.globalTs).localClock++;
+        int localClock = this.globalTs.localClock;
+        int id = this.globalTs.id;
+        return (new ScalarTimestamp(localClock, id));
+    }
+
+    @Override
+    public synchronized ScalarTimestamp updateAndIncrementTimeStamp(int SenderTimeStamp) {
+        this.globalTs.localClock = Math.max(this.globalTs.localClock, SenderTimeStamp);
+        this.globalTs.localClock++;
+
+        int localClock = this.globalTs.localClock;
+        int id = this.globalTs.id;
+        return (new ScalarTimestamp(localClock, id));
+    }
+
+    @Override
+    public ScalarTimestamp getCurrentTimestamp() {
+        return null;
+    }
+}
